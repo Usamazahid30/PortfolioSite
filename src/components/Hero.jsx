d1f77c1424891e2ab4c19afa6d701afe2ff3b70f
Muhammad Usama/IT/HO <zahidusama20@gmail.com>
2024-09-18 16:54:18 +0500

    Initial Build Complete

---
+++ src/components/Hero.jsx
@@ -0,0 +1,76 @@
+import { HERO_CONTENT } from "./../constants/index";
+import ProfilePic from "../assets/KevinRushProfile.jpg";
+import { motion } from "framer-motion";
+import { saveAs } from "file-saver";
+
+const Hero = () => {
+  const container = (delay) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5, delay: delay },
+    },
+  });
+  const downloadRes = () => {
+    saveAs("../../public/Usama-Resume.pdf", "Usama-resume.pdf");
+  };
+  return (
+    <div className="border-b border-neutral-900 pb-4  lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col  items-center  lg:items-start ">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl"
+            >
+              Muhammad Usama
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className=" bg-gradient-to-r from-pink-300  via-slate-500 to-purple-500  bg-clip-text  text-3xl tracking-tight text-transparent"
+            >
+              Full Stack Developer
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className=" my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+
+            <motion.button
+              className="mb-4 font-mono bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-400 hover:to-blue-600 text-white font-bold py-2 px-6 border-b-4 border-blue-800 hover:border-blue-600 rounded-lg shadow-lg transition-transform duration-200 ease-in-out"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 300 }}
+              onClick={downloadRes}
+            >
+              DOWNLOAD CV
+            </motion.button>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2  lg:p-8  ">
+          <div className=" flex justify-center">
+            <motion.img
+              className=" rounded-lg"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={ProfilePic}
+              alt="Profile"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,85 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `As a dedicated full-stack developer with two years of professional experience, I specialize in building high-performance, scalable web applications that prioritize both functionality and user experience. My expertise spans a comprehensive range of technologies, with a focus on front-end development using React and Next.js, and back-end systems leveraging Node.js, MySQL, PostgreSQL, and MongoDB. I excel at designing and implementing robust solutions that not only meet business objectives but also enhance user satisfaction through seamless interactions.
+
+Driven by a passion for problem-solving, I am committed to staying at the forefront of web development trends and continuously improving my skill set. My goal is to leverage my technical expertise and industry knowledge to deliver innovative, tailored solutions that propel business growth, improve operational efficiency, and create exceptional digital experiences.`;
+
+export const ABOUT_TEXT = `I am a skilled full-stack developer with a strong focus on delivering high-quality, scalable web applications that meet both business needs and user expectations. With two years of hands-on experience, I have developed expertise in front-end technologies like React and Next.js, as well as back-end frameworks such as Node.js, MySQL, PostgreSQL, and MongoDB. I take pride in crafting seamless, intuitive user interfaces and efficient, reliable server-side solutions. My passion for innovation drives me to stay current with industry trends and best practices, ensuring that the solutions I build are not only technically sound but also future-proof. I am dedicated to creating meaningful digital experiences that foster growth, efficiency, and success.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Software Engineer",
+    company: "TCS Private Limited.",
+    description: `Develop and maintain full-stack web applications using modern front-end frameworks (React, Next.js, Angular) and server-side technologies (Node.js, Express).`,
+    technologies: [
+      "Javascript",
+      "React.js",
+      "Next.js",
+      "Angular",
+      "Node js",
+      "mongoDB",
+      "SQL",
+    ],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "AMFCO International",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Javascript", "React"],
+  },
+  // {
+  //   year: "2021 - 2022",
+  //   role: "Full Stack Developer",
+  //   company: "Facebook",
+  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  // },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
+];
+
+export const PROJECTS = [
+  {
+    title: "Online Ticket Booking Site",
+    image: project1,
+    description:
+      "Our Match Ticket Booking Website is a specialized platform dedicated to facilitating the booking of tickets for matches. Cricket enthusiasts can now easily secure their seats for thrilling matches, from international tournaments to exciting league games, all through our user-friendly online platform Our website offers a seamless ticket booking experience, allowing users to effortlessly select their preferred matches and seats with just a few clicks. The intuitive booking process ensures a hassle-free transaction, from seat selection to payment confirmation.",
+    technologies: ["HTML", "CSS", "Javascript", "Tailwind", "React", ".NET"],
+  },
+  {
+    title: "Invoice-flow",
+    image: project2,
+    description:
+      "The Invoice-Flow Billing Management System is designed to efficiently manage large company ledgers, streamlining the billing and invoicing process. It automates the generation, tracking, and reconciliation of invoices, ensuring accuracy and reducing manual errors. The system provides real-time insights into financial transactions, helping businesses maintain control over outstanding payments and cash flow. With scalability and security at its core, this solution is ideal for handling high volumes of invoices while simplifying complex financial management tasks.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  // {
+  //   title: "Portfolio Website",
+  //   image: project3,
+  //   description:
+  //     "A personal portfolio website showcasing projects, skills, and contact information.",
+  //   technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  // },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "React", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  phoneNo: "+92 3069899782",
+  email: "usamazahid097@gmail.com",
+};
